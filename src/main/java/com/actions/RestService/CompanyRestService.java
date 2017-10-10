305becda7d4c5db80e94e04d5af0c44f44992743
Marouane Bouhouch <marouanebhch@gmail.com>
2017-10-10 18:08:36 +0100

    Attach tag to company

--- src/main/java/com/actions/RestService/CompanyRestService.java
+++ src/main/java/com/actions/RestService/CompanyRestService.java
@@ -18,14 +18,33 @@ public class CompanyRestService {
     @Autowired
     private TagRepository tagRepository;
 
-    @RequestMapping(value = "/companies/{company_id}/tags/{tag_id}", method = RequestMethod.PUT)
-    public ResponseEntity<Company> deleteTagFromCompany(@PathVariable("company_id") Long company_id, @PathVariable("tag_id") Long tag_id){
+    @RequestMapping(value = "/companies/{company_id}/removetag/{tag_id}", method = RequestMethod.PUT)
+    public ResponseEntity<Company> removeTagFromCompany(@PathVariable("company_id") Long company_id,
+                                                        @PathVariable("tag_id") Long tag_id){
         if (companyRepository.exists(company_id)){
             Company c = companyRepository.findOne(company_id);
             if(tagRepository.exists(tag_id)){
                 Tag tag = tagRepository.findOne(tag_id);
                 c.getTags().remove(tag);
                 companyRepository.save(c);
+            }
+            return new ResponseEntity<Company>(c, HttpStatus.OK);
+        }
+        else
+            return new ResponseEntity<Company>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/companies/{company_id}/attachtag/{tag_id}", method = RequestMethod.PUT)
+    public ResponseEntity<Company> attachTagToCompany(@PathVariable("company_id") Long company_id,
+                                                      @PathVariable("tag_id") Long tag_id){
+        if (companyRepository.exists(company_id)){
+            Company c = companyRepository.findOne(company_id);
+            if(tagRepository.exists(tag_id)){
+                Tag tag = tagRepository.findOne(tag_id);
+                if(c.getTags().indexOf(tag) != -1){
+                    c.getTags().add(tag);
+                    companyRepository.save(c);
+                }
             }
             return new ResponseEntity<Company>(c, HttpStatus.OK);
         }
